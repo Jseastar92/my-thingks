3194cd1aa2428aae2f6f9b35da300329f862a96f
JmSeastar <Jseastar92@gmail.com>
2019-07-18 03:37:20 +0900

    3rd Login page, logic

--- src/main/java/net/thingks/web/UserController.java
+++ src/main/java/net/thingks/web/UserController.java
@@ -1,11 +1,14 @@
 package net.thingks.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.thingks.domain.User;
@@ -19,6 +22,30 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "/user/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserId(userId);
+		if (user == null) {
+			System.out.println("Login Fail!");
+			return "redirect:/users/loginForm";
+		}
+		
+		if(!password.equals(user.getPassword())) {
+			System.out.println("Login Fail!");
+			return "redirect:/users/loginForm";
+		}
+		
+		System.out.println("Login Success");
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
+	}
+	
 	@GetMapping("/form")
 	public String form() {
 		return "/user/form";
@@ -30,7 +57,7 @@ public class UserController {
 		userRepository.save(user);
 		return "redirect:/users";
 	}
-	//Post users는 사용자 추가 , Getusesrs는 조회로 유추가능
+//	Post users는 사용자 추가 , Getusesrs는 조회로 유추가능
 	@GetMapping("")
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
@@ -47,7 +74,7 @@ public class UserController {
 		return "/user/updateForm";
 	}
 	
-	@PostMapping("/{id}")
+	@PutMapping("/{id}")
 	public String update(@PathVariable Long id, User newUser) {
 		User user = userRepository.findOne(id);
 		user.update(newUser);
