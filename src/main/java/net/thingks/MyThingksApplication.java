ae403688951fb5b4be9802c530360036ae00337b
JmSeastar <Jseastar92@gmail.com>
2019-07-17 21:45:06 +0900

    3rd section commit

---
+++ src/main/java/net/thingks/MyThingksApplication.java
@@ -0,0 +1,13 @@
+package net.thingks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyThingksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyThingksApplication.class, args);
+	}
+
+}
