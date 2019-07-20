82e0bd903e7fe1dfdd999148779eb12f45f9d64e
JmSeastar <Jseastar92@gmail.com>
2019-07-20 16:15:54 +0900

    final create profile page

--- src/main/java/net/thingks/web/UserController.java
+++ src/main/java/net/thingks/web/UserController.java
@@ -14,19 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import net.thingks.domain.User;
 import net.thingks.domain.UserRepository;
 
-
 @Controller
 @RequestMapping("/users")
 public class UserController {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@GetMapping("/loginForm")
 	public String loginForm() {
 		return "/user/login";
 	}
-	
+
 	@PostMapping("/login")
 	public String login(String userId, String password, HttpSession session) {
 		User user = userRepository.findByUserId(userId);
@@ -34,79 +33,93 @@ public class UserController {
 			System.out.println("Login Fail!");
 			return "redirect:/users/loginForm";
 		}
-		
-		if(!user.matchPassword(password)) {
+
+		if (!user.matchPassword(password)) {
 			System.out.println("Login Fail!");
 			return "redirect:/users/loginForm";
 		}
-		
+
 		System.out.println("Login Success");
 		session.setAttribute(HttpSessionUtils.USER_SESSION_KEY, user);
-		
+
 		return "redirect:/";
 	}
-	
+
 	@GetMapping("/logout")
 	public String logout(HttpSession session) {
 		session.removeAttribute(HttpSessionUtils.USER_SESSION_KEY);
-		
+
 		return "redirect:/";
 	}
-	
-	
+
 	@GetMapping("/form")
 	public String form() {
 		return "/user/form";
 	}
-	
+
 	@PostMapping("")
-	public String create(User user ) {
-		System.out.println("user :" + user );
+	public String create(User user) {
+		System.out.println("user :" + user);
 		userRepository.save(user);
 		return "redirect:/users";
 	}
+
 //	Post users는 사용자 추가 , Getusesrs는 조회로 유추가능
 	@GetMapping("")
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
 		return "/user/list";
 	}
-	
-	
 
 	@GetMapping("/{id}/form")
 	public String updateForm(@PathVariable Long id, Model model, HttpSession session) {
-		
-		if (HttpSessionUtils.isLoginUser(session)) {
+
+		if (!HttpSessionUtils.isLoginUser(session)) {
 			return "redirect:/users/loginForm";
 		}
-		
-		User sessionedUser = HttpSessionUtils.getUserFromSession(session) ;
-		if (sessionedUser.matchId(id)) {
+
+		User sessionedUser = HttpSessionUtils.getUserFromSession(session);
+		if (!sessionedUser.matchId(id)) {
 			throw new IllegalStateException("자신의 정보만 수정할 수 있습니다");
 		}
-		
+
 		User user = userRepository.findOne(id);
 		model.addAttribute("user", user);
 //		model.addAttribute("user", userRepository.findOne(id));
 		return "/user/updateForm";
 	}
-	
+
 	@PutMapping("/{id}")
 	public String update(@PathVariable Long id, User updatedUser, HttpSession session) {
-		if (HttpSessionUtils.isLoginUser(session)) {
+		if (!HttpSessionUtils.isLoginUser(session)) {
 			return "redirect:/users/loginForm";
 		}
-		
-		User sessionedUser = HttpSessionUtils.getUserFromSession(session) ;
-		if (sessionedUser.matchId(id)) {
+
+		User sessionedUser = HttpSessionUtils.getUserFromSession(session);
+		if (!sessionedUser.matchId(id)) {
 			throw new IllegalStateException("자신의 정보만 수정할 수 있습니다");
 		}
-	
+
 		User user = userRepository.findOne(id);
 		user.update(updatedUser);
 		userRepository.save(user);
 		return "redirect:/users";
 	}
-	
+
+	@GetMapping("/{id}/profile")
+	public String profile(@PathVariable Long id, Model model, HttpSession session) {
+		if (!HttpSessionUtils.isLoginUser(session)) {
+			return "redirect:/users/loginForm";
+		}
+		
+		User sessionedUser = HttpSessionUtils.getUserFromSession(session);
+		if (!sessionedUser.matchId(id)) {
+			throw new IllegalStateException("자신의 프로필을 조회해주세요.");
+		}
+		
+		User user = userRepository.findOne(id);
+		model.addAttribute("user", user);
+		return "/user/profile";
+	}	
+
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 
-INSERT INTO USER (ID,USER_ID, PASSWORD, NAME, EMAIL) VALUES (1,'seastar','111','해성','seastar@gg');
+INSERT INTO USER (ID,USER_ID, PASSWORD, NAME, EMAIL) VALUES (1,'seastar','111','해성','jseastar92@gmail.com');
 INSERT INTO USER (ID,USER_ID, PASSWORD, NAME, EMAIL) VALUES (2,'seastart','111','해성2','seastar2@gg');
 
-INSERT INTO QUESTION (id, writer_id, title, contents, create_date) VALUES (1, 1,'TEST1','TEST CONTENT',CURRENT_TIMESTAMP());
-INSERT INTO QUESTION (id, writer_id, title, contents, create_date) VALUES (2, 2,'Seastartㄱㅏ 쓴글','TEST CONTENT',CURRENT_TIMESTAMP());
+INSERT INTO QUESTION (id, writer_id, title, contents, create_date, count_of_answer) VALUES (1, 1,'SweetSpot 합격기원!','TEST CONTENT',CURRENT_TIMESTAMP(),0);
+INSERT INTO QUESTION (id, writer_id, title, contents, create_date, count_of_answer) VALUES (2, 2,'오늘 하루도 허쓸해봅시다!','TEST CONTENT',CURRENT_TIMESTAMP(),0);
